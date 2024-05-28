b9b2cc54eb8d5a19519078994bc9e70144815ed2
Samm Caagbay <sammcaag@gmail.com>
2024-05-28 17:43:43 +0800

    👍 Preparing the posters

--- landing.php
+++ landing.php
@@ -3,9 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/public/css/general.css">
+  <link rel="stylesheet" href="/public/css/navbar.css">
   <title>Document</title>
 </head>
 <body>
-  
+  <header>
+    <nav class="navbar">
+      <img src="/public/images/logo.png" alt="">
+      <h1 class="logo-label">FLICKS</h1>
+      <div class="search-bar">
+        <input class="search-bar" type="text" name="" id="" placeholder="Search for movies, showtimes, or cinemas...">
+        <button>
+          <img class="search-icon" src="/public//images/search.png" alt="">
+        </button>
+      </div>
+    </nav>
+  </header>
 </body>
 </html>
