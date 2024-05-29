cd492ca0e895a804078cac0286454c94fb0e734d
Samm Caagbay <sammcaag@gmail.com>
2024-05-29 12:44:28 +0800

    📊 Done Making the Navbar!!

--- landing.php
+++ landing.php
@@ -9,14 +9,24 @@
 </head>
 <body>
   <header>
-    <nav class="navbar">
-      <img src="/public/images/logo.png" alt="">
-      <h1 class="logo-label">FLICKS</h1>
-      <div class="search-bar">
-        <input class="search-bar" type="text" name="" id="" placeholder="Search for movies, showtimes, or cinemas...">
-        <button>
-          <img class="search-icon" src="/public//images/search.png" alt="">
-        </button>
+    <nav class="flex navbar">
+      <div class="flex logo-div">
+        <img src="/public/images/logo.png" alt="">
+        <h1 class="logo-label">FLICKS</h1>      
+      </div>
+
+      <div class="search-div flex">
+        <div class="search-bar">
+          <input class="search-input" type="text" name="" id="" placeholder="Search for movies, showtimes, or cinemas...">
+        </div>
+          <button class="search-button">
+            <img class="button-icon" src="/public//images/search.png" alt="">
+          </button>
+      </div>
+
+      <div class="login-div flex">
+        <h1 class="status">Log - in for Discounts!</h1>
+        <button class="login-button">Log-in</button>
       </div>
     </nav>
   </header>
