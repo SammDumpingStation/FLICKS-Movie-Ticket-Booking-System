5bccb6e5c9a764d31f8c18bb75a28bb25cc04668
Samm Caagbay <sammcaag@gmail.com>
2024-05-31 13:44:09 +0800

    👍 Book Ticket Section is Done!

--- book_ticket.php
+++ book_ticket.php
@@ -27,12 +27,14 @@
         <h1 class="title">Furiosa: A Mad Max Saga</h1>
         <div class="below-title">
           <h1 class="price">$400</h1>
-          <div class="setion">
+          <div class="timeslot">
             <h2 class="sec-head">Select Time Slot</h2>
             <h3 class="sec-desc">Select the time slot you wish to watch.</h3>
-            <button>12:30 P.M.</button>
-            <button>3:30 P.M.</button>
-            <button>6:30 P.M.</button>
+            <div class="times-container flex">
+              <button>12:30 P.M.</button>
+              <button>3:30 P.M.</button>
+              <button>6:30 P.M.</button>
+            </div>
           </div>
 
           <div class="tickets">
@@ -42,18 +44,25 @@
             <div class="quant-div">
               <h4 class="sect-title">Quantity</h4>
 
-              <div class="quantity-div">
+              <div class="quantity-div flex">
                 <h6 class="operators minus">-</h6>
-                <input type="number" class="quantity" name="" id="">
+                <input type="number" class="input" name="" id="">
                 <h6 class="operators plus">+</h6>
               </div>
+              <h6 class="info-quant">*You can buy a maximum of 8 tickets per transaction*</h6>
+
             </div>
 
             <div class="total-div">
               <h4 class="sect-title">Total Cost</h4>
-              <p class="total">$3200 + $40 (Booking fee) = <span class="total-plus-fee">$3240</span></p>
+              <p class="total">$3200 + $40 <span class="book-fee">(Booking fee)</span> = <span class="total-plus-fee">$3240.00</span></p>
             </div>
           </div>
+        </div>
+
+        <div class="button-operations">
+          <a href="movie_info.php"><button class="go-back">Go Back</button></a>
+          <a href="seat_selection.php"><button class="proceed">Proceed</button></a>
         </div>
       </div>
     </section>
