b9fc7acc8003f51aa7b243c545c08653f8ce36a8
Samm Caagbay <sammcaag@gmail.com>
2024-06-07 14:21:56 +0800

    👍 Pending Payments Done

--- source/Admin/landing.php
+++ source/Admin/landing.php
@@ -4,15 +4,128 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include_once '../../includes/css_links.php'?>
-  <link rel="stylesheet" href="../../public/css/landing.css">
+  <link rel="stylesheet" href="../../public/css/Admin/landing.css">
   <title>Document</title>
 </head>
 <body>
 
   <?php include_once '../../includes/navbar_admin.php';?>
 
-  <main>
-    <h1>Pending Payments</h1>
+  <main class="flex">
+    <section>
+      <h1>Pending Payments</h1>
+      <section class="pending-container">
+        <div class="pending-cinema">
+          <div>
+            <h3>Cinema 1</h3>
+            <p>Current Time</p>
+          </div>
+          <div>
+            <h2>Movie</h2>
+            <p>Furiosa: A Mad Max Saga</p>
+          </div>  
+          <div>
+            <h2>Time Start:</h2>
+            <p>12:30</p>
+          </div>   
+          <div>
+            <h2>Status</h2>
+            <p>Available</p>
+          </div>
+          <div>
+            <h2>Seats Available</h2>
+            <p>80 Seats</p>
+          </div>  
+          <div>
+            <h2>Pending</h2>
+            <p>12 Reservations</p>
+          </div>   
+        </div>
+        <div class="pending-cinema">
+          <div>
+            <h3>Cinema 2</h3>
+            <p>Current Time</p>
+          </div>
+          <div>
+            <h2>Movie</h2>
+            <p>Furiosa: A Mad Max Saga</p>
+          </div>  
+          <div>
+            <h2>Time Start:</h2>
+            <p>12:30</p>
+          </div>   
+          <div>
+            <h2>Status</h2>
+            <p>Available</p>
+          </div>
+          <div>
+            <h2>Seats Available</h2>
+            <p>80 Seats</p>
+          </div>  
+          <div>
+            <h2>Pending</h2>
+            <p>12 Reservations</p>
+          </div>            
+        </div>
+        <div class="pending-cinema">
+          <div>
+            <h3>Cinema 3</h3>
+            <p>Current Time</p>
+          </div>
+          <div>
+            <h2>Movie</h2>
+            <p>Furiosa: A Mad Max Saga</p>
+          </div>  
+          <div>
+            <h2>Time Start:</h2>
+            <p>12:30</p>
+          </div>   
+          <div>
+            <h2>Status</h2>
+            <p>Available</p>
+          </div>
+          <div>
+            <h2>Seats Available</h2>
+            <p>80 Seats</p>
+          </div>  
+          <div>
+            <h2>Pending</h2>
+            <p>12 Reservations</p>
+          </div>  
+        </div>
+        <div class="pending-cinema">
+          <div>
+            <h3>Cinema 4</h3>
+            <p>Current Time</p>
+          </div>
+          <div>
+            <h2>Movie</h2>
+            <p>Furiosa: A Mad Max Saga</p>
+          </div>  
+          <div>
+            <h2>Time Start:</h2>
+            <p>12:30</p>
+          </div>   
+          <div>
+            <h2>Status</h2>
+            <p>Available</p>
+          </div>
+          <div>
+            <h2>Seats Available</h2>
+            <p>80 Seats</p>
+          </div>  
+          <div>
+            <h2>Pending</h2>
+            <p>12 Reservations</p>
+          </div>  
+        </div>
+      </section> 
+
+      <section>
+        
+      </section>
+    </section>
+
   </main>
   
 </body>
