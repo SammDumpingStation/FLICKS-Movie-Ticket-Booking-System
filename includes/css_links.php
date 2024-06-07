39f0375b2073e4e7895e36a5711da2276e2cc820
Samm Caagbay <sammcaag@gmail.com>
2024-06-07 13:49:31 +0800

    👍 sEGREGATING CSS Files For Admin & Client

--- includes/css_links.php
+++ includes/css_links.php
@@ -1,4 +1,4 @@
   <link rel="stylesheet" href="/includes/font.css">
   <link rel="stylesheet" href="/includes/font.php">
-  <link rel="stylesheet" href="/public/css/general.css">
-  <link rel="stylesheet" href="/public/css/navbar.css">
+  <link rel="stylesheet" href="/public/css/Client/general.css">
+  <link rel="stylesheet" href="/public/css/Client/navbar.css">
---
+++ source/Admin/landing.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include_once '../../includes/css_links.php'?>
+  <link rel="stylesheet" href="../../public/css/landing.css">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php include_once '../../includes/navbar_admin.php';?>
+
+  <main>
+    <h1>Pending Payments</h1>
+  </main>
+  
+</body>
+</html>
