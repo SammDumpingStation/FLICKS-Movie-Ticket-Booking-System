8bfc61bfbb13dcc043d6f7dde5ef0d2d98f32e91
Samm Caagbay <sammcaag@gmail.com>
2024-06-09 10:44:15 +0800

    👍 Template Done For Paid Tickets History

--- source/Admin/paid_tickets_history.php
+++ source/Admin/paid_tickets_history.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include_once '../../includes/css_links.php'?>
-  <link rel="stylesheet" href="../../public/css/Admin/manage-movie.css">
+  <link rel="stylesheet" href="../../public/css/Admin/paid-tickets-history.css">
   <title>Document</title>
 </head>
 <body>
@@ -12,13 +12,14 @@
 
   <main>
     <a href="landing.php"><button class="return">Return</button></a>
-    <h1 class="title">Manage Movies</h1>
+    <h1 class="title">Paid Tickets History</h1>
     <section class="container">
       <div class="info-container">
         <div class="info-div">
-          <p class="grey p-info">Movie Title <span class="white">Furiosa: A Mad Max Saga</span></p>
-          <p class="grey p-info">Status: <span class="white">Now Showing</span></p>
-          <p class="grey p-info">Screen Location: <span class="white">Cinema 1</span></p>
+          <p class="grey p-info">Guest:<span class="white">Samm Reyven Joey Caagbay</span></p>
+          <p class="grey p-info">Movie:<span class="white">Furiosa: A Mad Max Saga</span></p>
+          <p class="grey p-info">Status:<span class="white">Paid</span></p>
+          <p class="grey p-info">Total Cost:<span class="white">$3240</span></p>
         </div>
         <div class="buttons">
           <button class="green-bg">Full Details</button>
