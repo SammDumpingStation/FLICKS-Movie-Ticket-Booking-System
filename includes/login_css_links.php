a7660bc7c63fa46efb8182cc9d8e48baae874af4
Samm Caagbay <sammcaag@gmail.com>
2024-06-09 15:26:16 +0800

    👍 Fixing Navbar Login button

---
+++ includes/login_css_links.php
@@ -0,0 +1,3 @@
+  <link rel="stylesheet" href="/includes/font.php">
+  <link rel="stylesheet" href="/public/css/general.css">
+  <link rel="icon" type="image/png" href="/public/images/logo.png">
---
+++ source/Log-in/log_in.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include_once '../../includes/login_css_links.php'?>
+  <title>Log - in</title>
+</head>
+<body>
+  
+</body>
+</html>
