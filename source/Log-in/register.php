a632a70c8856f74b6ea7761e38acc19319dbd622
Samm Caagbay <sammcaag@gmail.com>
2024-06-10 14:28:07 +0800

    👍 Register Templating Done!

---
+++ source/Log-in/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include_once '../../includes/login_css_links.php'?>
+  <link rel="stylesheet" href="../../public/css/Log-In/register.css">
+  <title>Log-in Landing Page</title>
+</head>
+<body>
+  <?php include_once '../../includes/login_logo.php'?>
+
+  <main>
+    <form class="main-form" action="log_in.php" method="get">
+      <h1 class="title">Are you a FLICKS Member?</h1>
+      <h2 class="title-desc">*Only registered members of FLICKS are allowed to have an account. Please verify if you are currently a member or not.*</h2>
+      <section class="main-input">
+        <label for="username" class="input-form">
+          <img src="../../public/images/user.png" alt="">
+          <input id="username" type="text" name="username" placeholder="Identification Number">
+        </label>
+        <button class="proceed">Check</button>
+        <p class="forgot">Not a member?</p>
+      </section>
+    </form>
+  </main>
+
+</body>
+</html>
