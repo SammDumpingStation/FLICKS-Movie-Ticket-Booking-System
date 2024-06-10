eefcfb841e706459a188fd6662444cac0ac57123
Samm Caagbay <sammcaag@gmail.com>
2024-06-10 20:11:47 +0800

    👍 Preparing the Identification Page

--- source/Log-in/register.php
+++ source/Log-in/register.php
@@ -11,15 +11,19 @@
   <?php include_once '../../includes/login_logo.php'?>
 
   <main>
-    <form class="main-form" action="log_in.php" method="get">
+    <form class="main-form" action="register.php" method="get">
       <h1 class="title">Are you a FLICKS Member?</h1>
       <h2 class="title-desc">*Only registered members of FLICKS are allowed to have an account. Please verify if you are currently a member or not.*</h2>
       <section class="main-input">
         <label for="username" class="input-form">
-          <img src="../../public/images/user.png" alt="">
+          <img src="../../public/images/id.png" alt="">
           <input id="username" type="text" name="username" placeholder="Identification Number">
         </label>
-        <button class="proceed">Check</button>
+        <div class="click-buttons">
+          <button name="operation" value="cancel" class="go-back">Cancel</button>
+          <button name="operation" value="Check" class="proceed">Check</button>
+        </div>
+        
         <p class="forgot">Not a member?</p>
       </section>
     </form>
